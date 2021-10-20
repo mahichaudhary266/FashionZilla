cf9e0a1cedb2c9bbf080119f2ba2e66dd04619bf
Kanesin <61185071+Kanesin@users.noreply.github.com>
2021-10-20 12:25:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Home | Fashionzilla</title>
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <!--link rel="stylesheet" href="header_nav_bar.css" type="text/css">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Crete+Round&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet"-->
+  </head>
+  <body>
+    <?php
+    require ("nav_bar.php");
+    ?>
+    <section id = "intro">
+      <div class = "large">
+      FashionZilla
+    </div>
+    <div class = "medium">
+      Shop Smarter
+    </div>
+      <text>Are you ready for an amazing online shopping experience? Check out FashionZilla, where you’ll find a great selection</text> <br> <text>at the most competitive prices.</text>
+    </section>
+    <section id = "products">
+      How you doin'?
+    </section>
+    <section id = "story">
+      <div class = "os">
+        our story
+      </div>
+      <text><center>Shopping online has never been more rewarding. Our selection of fashion goods includes stunning pieces for every occasion, size or budget. Our retail experts go far and beyond to create our breathtaking, creative collections, and our customer service team will make sure that your expectations are not just met, but exceeded.
+      <br><br>
+      Thanks to our innovative and service-oriented approach, FashionZilla has been highly successful since day one. Browse our site and get in touch if you need any assistance. Happy shopping!</center></text>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <?php
+      require ("footer.php");
+     ?>
+  </body>
+</html>
+
+<?php
+ ?>
